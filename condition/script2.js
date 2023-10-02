bded63d59e07e1f5b963baffa79961e07300678d
Palash69 <duttabanikpalash99@gmail.com>
2023-10-02 13:47:21 +0530

    02-10-2023 commit

--- condition/script2.js
+++ condition/script2.js
@@ -1,26 +1,51 @@
-var user;
+// let user;
 
-user = "admin";
-// user = "subadmin";
-// user = "testprep";
-// user = "user";
-// user = "abc";
+// user = "admin";
+// // user = "subadmin";
+// // user = "testprep";
+// // user = "user";
+// // user = "abc";
 
-switch (user) {
-  case "admin":
-    console.log("You get full access");
+//TODO: Switch case with String
+
+// switch (user) {
+//   case "admin":
+//     console.log("You get full access");
+//     break;
+//   case "subadmin":
+//     console.log("get access to create/delete courses");
+//     break;
+//   case "admin":
+//     console.log("get access to create/delete tests");
+//     break;
+//   case "admin":
+//     console.log("get access to consume content");
+//     break;
+
+//   default:
+//     console.log("Trial user");
+//     break;
+// }
+
+//TODO: Switch case with int
+
+let month = 3;
+
+switch (month) {
+  case 1:
+    console.log("Jan");
     break;
-  case "subadmin":
-    console.log("get access to create/delete courses");
+  case 2:
+    console.log("Feb");
     break;
-  case "admin":
-    console.log("get access to create/delete tests");
+  case 3:
+    console.log("Mar");
     break;
-  case "admin":
-    console.log("get access to consume content");
+  case 4:
+    console.log("Apr");
     break;
 
   default:
-    console.log("Trial user");
+    console.log("Default case.");
     break;
 }
